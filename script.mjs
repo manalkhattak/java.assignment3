6316dceb4c9b815ef72a8fb19637100579acb0c7
manalkhattak <manalmasood828@gmail.com>
2023-10-29 13:11:46 +0500

    yard to sq.feet converter

---
+++ script.mjs
@@ -0,0 +1,10 @@
+function convertyardToSquareFeet() {
+    // Get the value of yard input
+    var yard = parseFloat(document.getElementById("yard").value);
+    
+    // Conversion factor: 1 yard is equal to 9 square feet
+    var squareFeet = yard * 9;
+    
+    // Display the result on the screen
+    document.getElementById("result").innerHTML = yard + " yards = " + squareFeet + " Square Feet.";
+}
